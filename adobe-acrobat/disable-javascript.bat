687eaf9112c958bb1f07b4ab9098aa4a66f682f5
Luis <luisko58@t-online.de>
2025-07-03 15:38:24 +0200

    add first scripts

---
+++ adobe-acrobat/disable-javascript.bat
@@ -0,0 +1,5 @@
+reg add "HKLM\SOFTWARE\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" /v "bDisableJavaScript" /t REG_DWORD /d "0x00000001" /f
+
+reg add "HKLM\SOFTWARE\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" /v "bDisableTrustedFolders" /t REG_DWORD /d "0x00000001" /f
+
+PAUSE
---
+++ ms-teams/clear-teams-login.bat
@@ -0,0 +1,5 @@
+@RD /S /Q "%LocalAppData%\Packages\MSTeams_8wekyb3d8bbwe"
+@RD /S /Q "%LocalAppData%\Packages\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy"
+@RD /S /Q "%LocalAppData%\Microsoft\OneAuth"
+@RD /S /Q "%LocalAppData%\Microsoft\TokenBroker"
+@RD /S /Q "%LocalAppData%\Microsoft\IdentityCache"
